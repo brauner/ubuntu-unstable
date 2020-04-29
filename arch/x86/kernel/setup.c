16e8393ace710b98ade5888c8e6cc7e11eeb71ce
Seth Forshee <seth.forshee@canonical.com>
2020-04-29 10:44:48 -0500

    UBUNTU: SAUCE: (lockdown) Revert carried-forward lockdown patches
    Revert the patches carried forward from focal so they can be
    refreshed.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -18,7 +18,6 @@
 #include <linux/sfi.h>
 #include <linux/hugetlb.h>
 #include <linux/tboot.h>
-#include <linux/security.h>
 #include <linux/usb/xhci-dbgp.h>
 
 #include <uapi/linux/mount.h>
@@ -974,13 +973,6 @@ void __init setup_arch(char **cmdline_p)
 	if (efi_enabled(EFI_BOOT))
 		efi_init();
 
-	efi_set_secure_boot(boot_params.secure_boot);
-
-#ifdef CONFIG_LOCK_DOWN_IN_SECURE_BOOT
-	if (efi_enabled(EFI_SECURE_BOOT))
-		security_lock_kernel_down("EFI Secure Boot mode", LOCKDOWN_CONFIDENTIALITY_MAX);
-#endif
-
 	dmi_setup();
 
 	/*
@@ -1132,7 +1124,19 @@ void __init setup_arch(char **cmdline_p)
 	/* Allocate bigger log buffer */
 	setup_log_buf(1);
 
-	efi_set_secure_boot(boot_params.secure_boot);
+	if (efi_enabled(EFI_BOOT)) {
+		switch (boot_params.secure_boot) {
+		case efi_secureboot_mode_disabled:
+			pr_info("Secure boot disabled\n");
+			break;
+		case efi_secureboot_mode_enabled:
+			pr_info("Secure boot enabled\n");
+			break;
+		default:
+			pr_info("Secure boot could not be determined\n");
+			break;
+		}
+	}
 
 	reserve_initrd();
 
--- include/linux/efi.h
+++ include/linux/efi.h
@@ -42,8 +42,6 @@
 #define EFI_ABORTED		(21 | (1UL << (BITS_PER_LONG-1)))
 #define EFI_SECURITY_VIOLATION	(26 | (1UL << (BITS_PER_LONG-1)))
 
-#define EFI_IS_ERROR(x)		((x) & (1UL << (BITS_PER_LONG-1)))
-
 typedef unsigned long efi_status_t;
 typedef u8 efi_bool_t;
 typedef u16 efi_char16_t;		/* UNICODE character */
@@ -778,22 +776,6 @@ extern int __init efi_setup_pcdp_console(char *);
 #define EFI_MEM_ATTR		10	/* Did firmware publish an EFI_MEMORY_ATTRIBUTES table? */
 #define EFI_MEM_NO_SOFT_RESERVE	11	/* Is the kernel configured to ignore soft reservations? */
 #define EFI_PRESERVE_BS_REGIONS	12	/* Are EFI boot-services memory segments available? */
-#define EFI_SECURE_BOOT		13	/* Are we in Secure Boot mode? */
-
-enum efi_secureboot_mode {
-	efi_secureboot_mode_unset,
-	efi_secureboot_mode_unknown,
-	efi_secureboot_mode_disabled,
-	efi_secureboot_mode_enabled,
-};
-
-#ifdef CONFIG_EFI_PARAMS_FROM_FDT
-u32 __init efi_get__secure_boot(void);
-#else
-static inline u32 efi_get__secure_boot(void) {
-	return efi_secureboot_mode_unset;
-};
-#endif
 
 #ifdef CONFIG_EFI
 /*
@@ -804,8 +786,6 @@ static inline bool efi_enabled(int feature)
 	return test_bit(feature, &efi.flags) != 0;
 }
 extern void efi_reboot(enum reboot_mode reboot_mode, const char *__unused);
-
-extern void __init efi_set_secure_boot(enum efi_secureboot_mode mode);
 
 bool __pure __efi_soft_reserve_enabled(void);
 
@@ -833,8 +813,6 @@ efi_capsule_pending(int *reset_type)
 	return false;
 }
 
-static inline void efi_set_secure_boot(enum efi_secureboot_mode mode) {}
-
 static inline bool efi_soft_reserve_enabled(void)
 {
 	return false;
@@ -847,7 +825,6 @@ static inline bool efi_rt_services_supported(unsigned int mask)
 #endif
 
 extern int efi_status_to_err(efi_status_t status);
-extern const char *efi_status_to_str(efi_status_t status);
 
 /*
  * Variable Attributes
@@ -1106,6 +1083,12 @@ static inline bool efi_runtime_disabled(void) { return true; }
 extern void efi_call_virt_check_flags(unsigned long flags, const char *call);
 extern unsigned long efi_call_virt_save_flags(void);
 
+enum efi_secureboot_mode {
+	efi_secureboot_mode_unset,
+	efi_secureboot_mode_unknown,
+	efi_secureboot_mode_disabled,
+	efi_secureboot_mode_enabled,
+};
 enum efi_secureboot_mode efi_get_secureboot(void);
 
 #ifdef CONFIG_RESET_ATTACK_MITIGATION
